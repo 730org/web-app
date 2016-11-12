6fc1367a1a2bff3d27aa0f0c741be3663eadc06f
matt trask <mjftrask@gmail.com>
2016-11-12 12:31:17 -0600

    local politics table

---
+++ config/Migrations/20161112174714_local_politics.php
@@ -0,0 +1,42 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class LocalPolitics extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function create()
+    {
+        $table = $this->table('local_politics');
+
+        $table->addColumn('state', 'string')
+              ->addColumn('zip_code', 'integer')
+              ->addColumn('city', 'string')
+              ->addColumn('local_party_headquaters', 'string')
+              ->addColumn('contact_id', 'string')
+              ->addColumn('website', 'string')
+              ->addColumn('party', 'string')
+              ->addTimestamps()
+              ->create();
+    }
+}
